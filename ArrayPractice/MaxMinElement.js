f68abeb58ad1d78d249b660e657ba0e2a6617b25
SandyN90 <sandeepkumarnavik5@gmail.com>
2023-03-02 23:38:32 +0530

    Added max and min element of the array

---
+++ ArrayPractice/MaxMinElement.js
@@ -0,0 +1,12 @@
+// Find max and min element of the array.
+
+const arr = [2, 3, 1, 5, 7, 4, 8, 10];
+
+let min = max = arr[0];
+// let max;
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i]
+    if (arr[i] < min) min = arr[i];
+}
+console.log(max, min);
